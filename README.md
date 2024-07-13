468dff29d1b5fb40ae6d1c7809ee61b275f014fa
Laurens van Dijk <laurensrobertvandijk@gmail.com>
2024-07-13 08:36:12 +0200

    Add as seperate repo

---
+++ README.md
@@ -0,0 +1,1 @@
+# Teqit.NuGet
---
+++ Teqit.Extensions.Seq/ServiceCollectionExtensions.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+using Serilog.Events;
+
+namespace Teqit.Extensions.Seq;
+
+public static class ServiceCollectionExtensions
+{
+    /// <summary>
+    /// Add Serilog logging to SEQ.
+    /// </summary>
+    /// <param name="services">The dependency injection ServiceCollection object.</param>
+    /// <param name="url">The address and port on which the SEQ instance runs.</param>
+    /// <param name="level">The minimum logging level.</param>
+    /// <param name="apiKey">The API key.</param>
+    /// <returns>IServiceCollection</returns>
+    public static IServiceCollection AddSeq(this IServiceCollection services, string url = "http://localhost:5341", LogEventLevel level = LogEventLevel.Verbose, string? apiKey = null)
+    {
+        services.AddSerilog((logBuilder) =>
+        {
+            logBuilder.WriteTo.Seq(url, level, apiKey: apiKey)
+                .Enrich.WithEnvironmentName()
+                .Enrich.WithEnvironmentUserName()
+                .Enrich.WithMachineName()
+                .Enrich.WithProcessId()
+                .Enrich.WithProcessName()
+                .Enrich.WithThreadId()
+                .Enrich.WithThreadName();
+        });
+
+        return services;
+    }
+}
